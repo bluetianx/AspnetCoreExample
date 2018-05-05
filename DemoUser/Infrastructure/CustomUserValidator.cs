9db185567bb84c413dacf57e9b36fa42182a1e4b
BruceTian <brucetianx@163.com>
2018-05-05 16:50:39 +0800

    增加迁移文件

--- DemoUser/Infrastructure/CustomUserValidator.cs
+++ DemoUser/Infrastructure/CustomUserValidator.cs
@@ -1,7 +1,22 @@
-﻿namespace DemoUser.Infrastructure
+
+using System.Threading.Tasks;
+using DemoUser.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DemoUser.Infrastructure
 {
-    public class CustomUserValidator
+    public class CustomUserValidator : IUserValidator<AppUser>
     {
-        
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager,
+            AppUser user) {
+            if (user.Email.ToLower().EndsWith("test@163.com")) {
+                return Task.FromResult(IdentityResult.Success);
+            } else {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError {
+                    Code = "EmailError",
+                    Description = "仅仅可以包含test@163.com后缀"
+                }));
+            } }
+
     }
 }
